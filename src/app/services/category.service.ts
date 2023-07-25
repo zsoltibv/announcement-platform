243523a8d071f0bb09672c43059c4fc76f18c384
zsoltibv <zsoltibv@gmail.com>
2023-07-25 13:15:47 +0300

    added category service

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { Category } from "../models/category";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  baseURL: string = "https://newsapi20221108120432.azurewebsites.net/api/Categories"
+
+  readonly httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.baseURL);
+  }
+
+  getCategory(id: string): Observable<Category> {
+    return this.http.get<Category>(this.baseURL + '/' + id);
+  }
+}
